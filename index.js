eea5c7634d631c79f984ab8d729adf292727a5da
Amanda <amanda.simoes1707@gmail.com>
2021-03-22 20:44:19 -0300

    Adicionando as rotas

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const express = required('express');
-const cors = require('cors');
-const redis = require('./databases/redis')
+const express = require('express');
+const redis = require('./database/redis');
 
 const app = express();
-app.use(cors());
 
-app.listen(process.env.PORT, () => {
-    console.log('Servidor conectado')
-})
+
+app.listen(process.env.PORT, () => { 
+    console.log(`Server listening on port ${process.env.PORT}`); 
+});
